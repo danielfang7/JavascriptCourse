8f2dacdc7476b6290680277395a3ba773c10870f
Daniel Fang <daniel@gladetool.com>
2024-05-25 12:11:18 -0700

    operators, let const var, data types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,17 @@
-let country = "United States";
-let continent = "North America";
+// Assignment 1
+const country = "United States";
+const continent = "North America";
 let population = "341";
 
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Assignment 2
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+// Assignment 3
+language = "English";
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,47 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 console.log("Jonas");
 console.log("23");
 
+// Variable name conventions
 let firstName = "Jonas";
 console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+// Undefined Example
+let year;
+console.log(year);
+console.log(typeof year);
+*/
+
+// Math operators
+const now = 2024;
+const ageDaniel = now - 1997;
+const ageSarah = now - 2018;
+console.log(ageDaniel, ageSarah);
+
+// String concatentation with +
+const firstName = "Daniel";
+const lastName = "Fang";
+console.log(firstName + " " + lastName);
+
+// Assignment Operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+console.log(x);
+
+// Comparison operators 
+console.log(ageDaniel > ageSarah);
+console.log(ageSarah >= 6);
+const isOverSix = ageSarah >= 6;

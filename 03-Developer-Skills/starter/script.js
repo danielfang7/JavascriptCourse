2fccbfe3309238f732b759508cae376201b42674
Daniel Fang <daniel@gladetool.com>
2024-06-03 02:09:31 -0700

    editor and dev tool installations

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xyz = 26;
+
+if (xyz === 23) {
+  console.log(23);
+} else {
+  console.log(`no`);
+}
+
+const calcAge = (birthYear) => 2024 - birthYear;
+
+console.log(calcAge(1997));

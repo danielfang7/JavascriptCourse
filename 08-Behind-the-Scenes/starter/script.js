368494fb9df45090b8323a5196c749015c4513bb
Daniel Fang <danielfang7@gmail.com>
2024-06-07 01:13:14 -0700

    starting project 8 behind the scenes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2024 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = `Daniel`;
+calcAge(1997);

4b7a9772dfc62981cba1400dbe6f8a6a85aafee8
Daniel Fang <daniel@gladetool.com>
2024-07-07 02:58:07 -0700

    event delegation

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,7 +32,56 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//////////////////////////////
+// Button Scrolling
+btnScrollTo.addEventListener(`click`, function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+  console.log(`Current scroll (X/Y)`, window.pageXOffset, pageYOffset);
+  console.log(document.documentElement.clientHeight);
+  console.log(document.documentElement.clientWidth);
+
+  // Scrolling (current position + current scroll)
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // // Smooth Scroll
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: `smooth`,
+  // });
+
+  // New method of Scroll - way easier, only works in Modern Browsers
+  section1.scrollIntoView({ behavior: `smooth` });
+});
+
 ///////////////////////////////
+// Page Navigation
+// document.querySelectorAll(`.nav__link`).forEach(function (el) {
+//   el.addEventListener(`click`, function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute(`href`);
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+//   });
+// });
+
+// Event Delegation Method
+document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
+  e.preventDefault();
+  // Matching Strategy (only select link elements)
+  if (e.target.classList.contains(`nav__link`)) {
+    const id = e.target.getAttribute(`href`);
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+  }
+});
+
+/////////////////////////////////
 /*
 // Selecting Entire HTML
 console.log(document.documentElement);
@@ -151,7 +202,7 @@ h1.addEventListener(`mouseenter`, alertH1);
 // h1.onmouseenter = function (e) {
 //   alert(`addEventListener: Great! You are reading the heading :D #2`);
 // };
-*/
+
 
 // rgb(255, 255 ,255)
 const randomInt = (min, max) =>
@@ -178,3 +229,4 @@ document.querySelector(`.nav`).addEventListener(`click`, function (e) {
   this.style.backgroundColor = randomColor();
   console.log(`NAV BAR`, e.target);
 });
+*/
